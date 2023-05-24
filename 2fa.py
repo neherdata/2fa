45f33e0e60cb3810d8dbace3704d1cbe7136cd45
Tyler M. Neher <tyler@neherdata.com>
2023-05-24 07:24:52 -0400

    up
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- 2fa.py
+++
@@ -1,5 +0,0 @@
-from urls import urls
-
-# print(url_tfa_supporting_sites)
-
-print(urls.url_tfa_supporting_sites)
--- urls.py
+++ urls.py
@@ -1,12 +1,9 @@
-
-def urls():
-	url_all = "https://api.2fa.directory/v3/all.json"
-	url_tfa_supporting_sites = "https://api.2fa.directory/v3/tfa.json"
-	url_sms = "https://api.2fa.directory/v3/sms.json"
-	url_call = "https://api.2fa.directory/v3/call.json"
-	url_email = "https://api.2fa.directory/v3/email.json"
-	url_custom_hardware = "https://api.2fa.directory/v3/custom-hardware.json"
-	url_totp = "https://api.2fa.directory/v3/totp.json"
-	url_u2f = "https://api.2fa.directory/v3/u2f.json"
-	url_custom_software = "https://api.2fa.directory/v3/custom-software.json"
-
+url_all = "https://api.2fa.directory/v3/all.json"
+url_tfa_supporting_sites = "https://api.2fa.directory/v3/tfa.json"
+url_sms = "https://api.2fa.directory/v3/sms.json"
+url_call = "https://api.2fa.directory/v3/call.json"
+url_email = "https://api.2fa.directory/v3/email.json"
+url_custom_hardware = "https://api.2fa.directory/v3/custom-hardware.json"
+url_totp = "https://api.2fa.directory/v3/totp.json"
+url_u2f = "https://api.2fa.directory/v3/u2f.json"
+url_custom_software = "https://api.2fa.directory/v3/custom-software.json"
